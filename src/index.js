2f9f06498eea966eb9dfb9294368d36004315ae5
Vladimir Kostroba <vladimirkostroba22@gmail.com>
2024-02-06 05:51:04 +0100

    commit

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import refs from "./js/refs";
+
+
+if (document.documentElement.clientWidth < 1200) {
+   refs.openModalBtn.addEventListener('click', openModal);
+   refs.closeModalBtn.addEventListener('click', closeModal);
+}
+
+refs.scrollUpBtn.addEventListener('click', scrollUp);
+
+// ///
+
+function openModal(e){
+    if(document.documentElement.clientWidth < 1200){
+        refs.modal.classList.add("open-modal");
+        refs.closeModalBtn.classList.remove('closed-btn');
+    }
+}
+
+function closeModal(){
+    refs.modal.classList.remove("open-modal");
+    refs.closeModalBtn.classList.add('closed-btn');
+}
+
+// 
+
+function scrollUp(){
+    window.scrollTo(0, 0);
+}
---
+++ src/js/refs.js
@@ -0,0 +1,8 @@
+export default  
+
+refs = {
+openModalBtn:document.querySelector('#open-modal'),
+closeModalBtn:document.querySelector('#close-modal'),
+modal:document.querySelector('.js-header__modal'),
+scrollUpBtn:document.querySelector('.scroll-up'),
+}
