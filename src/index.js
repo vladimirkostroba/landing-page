d6c13492183fcdf497e15fc2f39439999e4472b4
Vladimir Kostroba <vladimirkostroba22@gmail.com>
2024-02-06 06:04:01 +0100

    commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
-import refs from "./js/refs";
+const refs = {
+    openModalBtn:document.querySelector('#open-modal'),
+    closeModalBtn:document.querySelector('#close-modal'),
+    modal:document.querySelector('.js-header__modal'),
+    scrollUpBtn:document.querySelector('.scroll-up'),
+    }
 
 
 if (document.documentElement.clientWidth < 1200) {
--- src/js/refs.js
+++
@@ -1,8 +0,0 @@
-export default  
-
-refs = {
-openModalBtn:document.querySelector('#open-modal'),
-closeModalBtn:document.querySelector('#close-modal'),
-modal:document.querySelector('.js-header__modal'),
-scrollUpBtn:document.querySelector('.scroll-up'),
-}
